d9d98d6b9d3a923ffbc2b6f326091861f8efccef
brittonhayes <brittonhayes@users.noreply.github.com>
2024-02-08 01:14:10 -0800

    fix: change auth check mechanism to be based on any errors with bot_id cookie

--- internal/service/home.go
+++ internal/service/home.go
@@ -17,11 +17,9 @@ type HomeData struct {
 func (s *Service) GetHomePage(w http.ResponseWriter, r *http.Request) *api.Response {
 	tmpl := template.Must(template.ParseFS(notionstix.TEMPLATES, "web/*.html"))
 
-	botCookie, _ := cookies.ReadEncrypted(r, "bot_id", []byte(s.cookieSecret))
-	s.logger.Info("Bot Cookie", botCookie != "")
-
+	_, err := cookies.ReadEncrypted(r, "bot_id", []byte(s.cookieSecret))
 	tmpl.ExecuteTemplate(w, "home", HomeData{
-		Authenticated:  botCookie != "",
+		Authenticated:  err == nil,
 		IntegrationURL: "https://api.notion.com/v1/oauth/authorize?owner=user&client_id=080c1454-5a25-43af-b5ab-06162b1955d9&redirect_uri=https%3A%2F%2Fnotion-stix.up.railway.app%2Fauth%2Fnotion%2Fcallback&response_type=code",
 	})
 
