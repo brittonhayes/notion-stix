947847b5455399de6c333fc9cfbc11c6fe95828a
brittonhayes <brittonhayes@users.noreply.github.com>
2024-02-07 16:19:25 -0800

    fix: remove csp and re-add allowed hosts

--- internal/server/server.go
+++ internal/server/server.go
@@ -45,13 +45,11 @@ func New(ctx context.Context, config *Config) *Server {
 
 	r := chi.NewRouter()
 	secureMiddleware := secure.New(secure.Options{
-		// AllowedHosts:          []string{"railway.app", "notion-stix.up.railway.app", "www.notion.so", "api.notion.com"},
-		HostsProxyHeaders:     []string{"X-Forwarded-Host"},
-		SSLRedirect:           true,
-		FrameDeny:             true,
-		ContentTypeNosniff:    true,
-		BrowserXssFilter:      true,
-		ContentSecurityPolicy: "script-src $NONCE",
+		AllowedHosts:       []string{"railway.app", "notion-stix.up.railway.app", "www.notion.so", "api.notion.com"},
+		SSLRedirect:        true,
+		FrameDeny:          true,
+		ContentTypeNosniff: true,
+		BrowserXssFilter:   true,
 	})
 
 	r.Use(middleware.Heartbeat("/healthz"))
